9412a309bdefd7ff19ebdeb55c2d19c5ff67bf8f
kyeongrok.kim <kyeongrokkim@okiconcession.com>
2017-08-04 13:33:52 +0900

    []와 new Array()

---
+++ 003.js
@@ -0,0 +1,9 @@
+
+//javascript Array 만들기
+// [] 를 이용하는 방법
+let numbers = [1, 2, 3];
+let strings = ["hello", "bye", "welcome"];
+
+// new Array()를 이용하는 방법. []를 이용하는 방법과 동일하다. []를 이용하는게 좋다.
+let numbers2 = new Array(1, 2, 3);
+let strings2 = new Array("hello", "bye", "welcome");
