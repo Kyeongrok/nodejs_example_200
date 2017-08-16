fea6b49e0ef60ee17eb77ab7ccddee9af626c4b4
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-08-16 13:30:39 +0900

    012_switch 문

--- 001to020_level1/009_for.js
+++ 001to020_level1/009_for.js
@@ -1,6 +1,19 @@
 // 구구단을 활용한 for 반복문
-let number = 9;
+/*let number = 9;
 
 for (var i = 1; i < 10; i++) {
   console.log(number * i);
 }
+*/
+// for을 활요하지 않을 경우
+let number = 9;
+
+console.log(number * 1);
+console.log(number * 2);
+console.log(number * 3);
+console.log(number * 4);
+console.log(number * 5);
+console.log(number * 6);
+console.log(number * 7);
+console.log(number * 8);
+console.log(number * 9);
