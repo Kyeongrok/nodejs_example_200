2f68b38fa2cd0a5ca785ea174723fd85208babc6
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-09-12 01:44:45 +0900

    chapter 23, 24

---
+++ 021to080_level2/023_incrementOperator.js
@@ -0,0 +1,9 @@
+// 201709_JJH 증감 연산자 Javascript + Jquery P.50 참고
+let number = 1;
+
+console.log(++number);
+console.log(number++);
+console.log(number);
+console.log(number--);
+console.log(number);
+console.log(--number);
---
+++ 021to080_level2/024_conditionalTernaryOperator.js
@@ -0,0 +1,6 @@
+// 201709_JJH 삼항연산자 Javascript Jquery 입문 P. 89 참고
+
+let num_1 = 1;
+let num_2 = 2;
+
+(num_1 > num_2) ? console.log("num_1이 더 큽니다") : console.log("num_2가 더 큽니다");
