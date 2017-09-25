c219023ed794392f43e43c9e67af20d98986bb24
kyeongrok.kim <kyeongrokkim@okiconcession.com>
2017-09-25 18:10:06 +0900

    sort 추가

--- 001to030_level1/017_arraryModify2.js
+++ 001to030_level1/017_arraryModify2.js
@@ -1,2 +1,35 @@
 // 201709_JJH / 배열 조작 매서드 - 배열 정렬하기 sort() 메서드, P.68
 
+const fields = ['timeoutsRemaining',
+  'flagrantFouls',
+  'defensive3Seconds',
+  'jumpshots',
+  'dunks',
+  'layups',
+  'tipins',
+  'hooks',
+  'pointsOffTurnovers',
+  'pointsInPaint',
+  'secondChancePoints',
+  'fastBreakPoints',
+  'biggestLead',
+  'minutes',
+  'fieldGoals',
+  'freeThrows',
+  'ejections',
+  'threePointFieldGoals',
+  'points',
+  'rebounds',
+  'assists',
+  'steals',
+  'blockedShots',
+  'turnovers',
+  'personalFouls',
+  'disqualifications',
+  'technicalFouls'];
+
+
+console.log(fields);
+const sortedFields = fields.sort();
+
+console.log(sortedFields);
