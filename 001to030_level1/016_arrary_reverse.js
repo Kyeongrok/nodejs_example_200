0a4b3f486b54479e6502ff8f0fc4b9aa8e62163f
kyeongrok <oceanfog1@gmail.com>
2017-09-26 05:48:01 +0900

    016

---
+++ 001to030_level1/016_arrary_reverse.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4];
+numbers.reverse();
+
+console.log('numbers:', numbers);
+
+const stringArray = 'hello'.split('');
+stringArray.reverse();
+console.log('stringArray:', stringArray);
