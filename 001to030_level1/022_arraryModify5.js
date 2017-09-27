a4e42ad45a86b8718ae04b8dc973611f7c4941b7
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-09-27 19:05:54 +0900

    022_arrary modify5.js

--- 001to030_level1/022_arraryModify5.js
+++ 001to030_level1/022_arraryModify5.js
@@ -1,1 +1,12 @@
 // 201709_JJH / 배열 조작 매서드 - 첫번째 배열 제거 및 추가하기 shift() unshift() 메서드, P.70
+let destination = ['런던', '파리', '로마'];
+
+console.log(destination);
+
+//shift
+console.log(destination.shift());
+console.log(destination);
+
+// unshift
+destination.unshift('뉴욕');
+console.log(destination);
