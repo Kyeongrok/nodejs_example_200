d0fa9dbe4e22e3e06b4a66fec560bec2c6727cac
kyeongrok <oceanfog1@gmail.com>
2017-10-06 21:32:59 +0900

    mod

--- 001to030_level1/004_print_hello.js
+++ 001to030_level1/004_print_hello.js
@@ -1,9 +1,3 @@
 console.log('hello');
 console.log('hello', 'bye'); // 여러개 값 출력하기
 console.warn(`this line ${'can make error'} `);
-
-
-
-
-
-
---
+++ 001to030_level1/011_mod.js
@@ -0,0 +1,8 @@
+console.log('5 % 2 :', 5 % 2);
+
+const numbers = [5, 6, 7, 8];
+
+console.log(numbers[0], 'is even number:', numbers[0] % 2 === 0);
+console.log(numbers[1], 'is even number:', numbers[1] % 2 === 0);
+console.log(numbers[2], 'is even number:', numbers[2] % 2 === 0);
+console.log(numbers[3], 'is even number:', numbers[3] % 2 === 0);
