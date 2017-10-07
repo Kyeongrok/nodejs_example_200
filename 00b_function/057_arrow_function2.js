56a69155b894056ecffbefd226680dc49c22a0bd
kyeongrok <oceanfog1@gmail.com>
2017-10-07 10:38:11 +0900

    arrow_function2

---
+++ 00b_function/057_arrow_function2.js
@@ -0,0 +1,20 @@
+// define
+const printHello = () => {
+  console.log('hello');
+};
+
+const printMessage = (message) => {
+  console.log(message);
+};
+const plus = (a, b) => {
+  return a + b;
+}
+const minus = (a, b) => {
+  return a - b;
+}
+
+// print
+printHello();
+printMessage('message');
+console.log('plus :', plus(10, 20));
+console.log('minus :', minus(10, 20));
--- 00c_functional_programming/118_composit_function.js
+++ 00c_functional_programming/118_composit_function.js
@@ -1,5 +1,12 @@
 const multiple5 = x => x * 5;
 const add10 = x => x + 10;
 
+const plus = (a, b) => a + b;
+const minus = (a, b) => a - b;
+
+
 // (20 + 10) * 5
 console.log(multiple5(add10(20)));
+
+// (10 + 20) - 40
+console.log(minus(plus(10, 20), 40));
