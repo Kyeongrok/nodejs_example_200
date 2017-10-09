77b3edf5af68978c3e28993fc2621455032808f9
kyeongrok <oceanfog1@gmail.com>
2017-10-09 19:12:25 +0900

    split

---
+++ 031to080_level2/042_split.js
@@ -0,0 +1,12 @@
+const dateString = '2017-08-24 11:00:00';
+
+// split
+const date = dateString.split(' ')[0];
+const time = dateString.split(' ')[1];
+
+// print
+console.log('date : %s', date);
+console.log('time : %s', time);
+
+const result = 'abc'.split('b');
+console.log('result', result);
