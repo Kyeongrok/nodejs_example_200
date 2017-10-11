7e399f12dbc111333bfbf32a1acd47825646720a
kyeongrok.kim <kyeongrokkim@okiconcession.com>
2017-10-11 22:16:03 +0900

    conditional_ternary

--- 00a_basic/023_logical_operators.js
+++ 00a_basic/023_logical_operators.js
@@ -1,13 +1,18 @@
-const studentList = [
-  { name: 'kyeongrok', age: 31, score: 85 },
-  { name: 'jihyun', age: 31, score: 95 },
-  { name: 'minsup', age: 35, score: 76 },
-];
+const value30 = 30;
+const value50 = 50;
 
-const kyeongrok = studentList[0].age > 30 && studentList[0].score >= 90;
-const jihyun = studentList[1].age > 30 && studentList[1].score >= 90;
-const minsup = studentList[2].age > 30 || studentList[2].score >= 90;
+const andTrueTrue = value30 >= 30 && value50 >= 30;
+const andTrueFalse = value30 >= 30 && value50 >= 100;
+const andFalseFalse = value30 >= 40 && value50 >= 100;
 
-console.log('kyeongrok: age > 30 and score >= 90 | ', kyeongrok);
-console.log('jihyun: age > 30 and score >= 90 | ', jihyun);
-console.log('minsup: age > 30 or score >= 90 | ', minsup);
+const orTrueTrue = value30 >= 30 || value50 >= 30;
+const orTrueFalse = value30 >= 30 || value50 >= 100;
+const orFalseFalse = value30 >= 40 || value50 >= 100;
+
+console.log('andTrueTrue:', andTrueTrue);
+console.log('andTrueFalse:', andTrueFalse);
+console.log('andFalseFalse:', andFalseFalse);
+console.log('---------------------------');
+console.log('orTrueTrue:', orTrueTrue);
+console.log('orTrueFalse:', orTrueFalse);
+console.log('orFalseFalse:', orFalseFalse);
--- 00a_basic/024_conditional_ternary_operator.js
+++ 00a_basic/024_conditional_ternary_operator.js
@@ -1,7 +1,10 @@
 const num1 = 1;
 const num2 = 2;
+const list = [1, 2, 3, 4];
+const emptyList = [];
 
-const result = (num1 > num2) ? 'num1' : 'num2';
-
+const result = num1 > num2 ? 'num1' : 'num2';
 console.log(result, '이(가) 더 큽니다.');
 
+list.length > 0 ? console.log(list) : console.log('list가 비었습니다.');
+emptyList.length > 0 ? console.log(list) : console.log('list가 비었습니다.');
