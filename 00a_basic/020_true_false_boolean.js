4268513a8645d04cf24b6041bd23423c7dcba883
kyeongrok.kim <kyeongrokkim@okiconcession.com>
2017-10-11 20:25:31 +0900

    true, false, increasement operator

--- 00a_basic/020_true_false_boolean.js
+++ 00a_basic/020_true_false_boolean.js
@@ -1,3 +1,12 @@
+console.log('10 > 20:', 10 > 20);
+console.log('30 > 20:', 30 > 20);
+
+const isTrue = true;
+const isFalse = false;
+
+console.log('isTrue:', isTrue);
+console.log('isFalse:', isFalse);
+
 console.log('typeof (true):', typeof (true));
 console.log('typeof (false):', typeof (false));
 
--- 00a_basic/021_increment_operator.js
+++ 00a_basic/021_increment_operator.js
@@ -1,9 +1,11 @@
-// 201709_JJH 증감 연산자 Javascript + Jquery P.50 참고
 let number = 1;
 
-console.log(++number);
-console.log(number++);
-console.log(number);
-console.log(number--);
-console.log(number);
-console.log(--number);
+console.log('number :', number);
+number += 1;
+console.log('after number += 1 :', number);
+number -= 1;
+console.log('after number -= 1 :', number);
+number += 10;
+console.log('after number += 10 :', number);
+number -= 5;
+console.log('after number -= 5 :', number);
