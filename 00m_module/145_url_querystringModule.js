bb9d5b590d4fbf285305ab68338d2908b291a7e0
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-10-17 00:56:10 +0900

    내장 모듈 파일 위치 변경 및 번호 rename

--- 00m_module/145_url_querystringModule.js
+++ 00m_module/145_url_querystringModule.js
@@ -1,18 +1,17 @@
-// 201709_JJH url
 //  URL주소와 요청 파라미터 그리고 요청 파라미터 확인하기
-let url = require('url');
-let querystrying = require('querystring');
+const url = require('url');
+const querystrying = require('querystring');
 
 // 주소를 URL 객체로 만듬
-let urlAddress = url.parse('https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=nodejs+%EB%9E%80');
+const urlAddress = url.parse('https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=nodejs+%EB%9E%80');
 
 // URL 객체를 문자열로 만들기
-let urlStr = url.format(urlAddress);
+const urlStr = url.format(urlAddress);
 
-console.log('url 주소 : %s', urlStr );
+console.log('url 주소 : %s', urlStr);
 console.log(urlAddress);
 
-let queryValue = querystrying.parse(urlAddress.query);
+const queryValue = querystrying.parse(urlAddress.query);
 
 console.log('질문 : %s', queryValue.query);
 console.log('요청 한 질의 값 :%s', querystrying.stringify(queryValue));
