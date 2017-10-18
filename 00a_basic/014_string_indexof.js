8e7a2f9395a45c08d7d1dc9a3046edaef4f1c119
kyeongrok.kim <kyeongrokkim@okiconcession.com>
2017-10-18 15:00:58 +0900

    014 hel라인 추가

--- 00a_basic/014_string_indexof.js
+++ 00a_basic/014_string_indexof.js
@@ -2,6 +2,7 @@ const string = 'hello';
 const string2 = 'helelelelelello';
 const string3 = 'The method then selects elements from the start argument.';
 
+console.log('hel:', string.indexOf('hel'));
 console.log('el:', string.indexOf('el'));
 console.log('elelelel:', string2.indexOf('el'));
 console.log('method:', string3.indexOf('method'));
