ba1cc1adb17b497af31c14d31ffc2e1715a54eae
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-10-19 10:09:52 +0900

    149_http_Module 모듈

--- 00m_module/http/149_http_Module.js
+++ 00m_module/http/149_http_Module.js
@@ -4,7 +4,20 @@ const http = require('http');
 
 const server = http.createServer();
 
-server.listen(49955, () => {
+// 이벤트 연결
+server.on('request', () => {
+  console.log('Request');
+});
+
+server.on('connection', () => {
+  console.log('Connection');
+});
+
+server.on('close', () => {
+  console.log('Close');
+});
+
+server.listen(50000, () => {
   console.log('서버가 동작 중입니다, http://127.0.0.1:50000');
 });
 
@@ -13,3 +26,6 @@ const testClose = function () {
   server.close();
   console.log('서버가 종료되었습니다, http://127.0.0.1:50000');
 };
+
+setTimeout(testClose, 10000);
+
