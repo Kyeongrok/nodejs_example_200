768ce8857ad14ec6a841c0e7150f794ecfa4f3f5
kyeongrok.kim <kyeongrokkim@okiconcession.com>
2017-10-19 16:19:03 +0900

    curring_ex

--- 00k_functional_programming/120_curring_example.js
+++ 00k_functional_programming/120_curring_example.js
@@ -1,4 +1,12 @@
-const add = x => y => x + y;
-const add10 = add(10);
+const fs = require('fs');
 
-console.log(add10(20));
+const openFileAndPrint = path => fileName => fs.readFile(path + fileName, (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
+
+const thisDirOpenFileAndPrint = openFileAndPrint('./');
+const otherDirOpenFileAndPrint = openFileAndPrint('../');
+
+thisDirOpenFileAndPrint('110_sum.js');
+otherDirOpenFileAndPrint('package.json');
