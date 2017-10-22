58020ef4066dd2688ba9fa779f00173b6a1ce7dc
kyeongrok <oceanfog1@gmail.com>
2017-10-23 01:33:57 +0900

    regex{}

--- 00c_regexp/047_1_regexp_dot.js
+++ 00c_regexp/047_1_regexp_dot.js
@@ -1,6 +1,6 @@
 const string = '<h1>특별가 - </h1><h2>99,000원</h2>';
-const result1 = string.replace(/<..>/g, '');
-const result2 = string.replace(/<.../g, '');
+const replaceH1 = string.replace(/<.{0,1}h1>/g, '');
+const replaceHTag = string.replace(/<.{0,1}h.>/g, '');
 
-console.log('<..> :', result1);
-console.log('<... :', result2);
+console.log(replaceH1);
+console.log(replaceHTag);
