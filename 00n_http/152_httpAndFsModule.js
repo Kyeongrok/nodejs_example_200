a0154f692bfb4f407831c0a5d18ddf81f5716478
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-10-22 16:56:28 +0900

    151_http_response.js, 152_httpAndFsModule, example.html

--- 00n_http/152_httpAndFsModule.js
+++ 00n_http/152_httpAndFsModule.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const http = require('http');
 
-http.createServer((req, res) => {
-  fs.readFile('example,html', (error, data) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(data);
+http.createServer((request, response) => {
+  fs.readFile('./example.html', (error, data) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end(data);
   });
 }).listen(50000, () => {
   console.log('서버가 동작 중입니다, http://127.0.0.1:50000');
