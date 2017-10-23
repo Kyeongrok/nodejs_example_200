6a2e073544a5a32003df0b62420f56cf875f3ac2
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-10-23 23:56:55 +0900

    156_http_requestMethodPost

--- 00n_http/156_http_requestMethodPost.js
+++ 00n_http/156_http_requestMethodPost.js
@@ -1,15 +1,21 @@
 const http = require('http');
-const url = require('url');
+const fs = require('fs');
 
 http.createServer((request, response) => {
-  const get = url.parse(request.url, true).query;
+  // const get = url.parse(request.url, true).query;
 
   if (request.method === 'GET') {
-    response.writeHead(200, { 'Context-Type': 'text/html' });
-    response.end(`<h1>${JSON.stringify(get)}</h1>`);
-    console.log(`${request.method}방식의 요청입니다`);
+    fs.readFile('./example_2.html', (error, data) => {
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.end(data);
+      console.log(`${request.method}방식의 요청입니다`);
+    });
   } else if (request.method === 'POST') {
-    console.log(`${request.method}방식의 요청입니다`);
+    request.on('data', (data) => {
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      response.end(data);
+      console.log(`${request.method}방식의 요청입니다`);
+    });
   }
 }).listen(50000, () => {
   console.log('서버가 동작 중입니다, http://127.0.0.1:50000');
