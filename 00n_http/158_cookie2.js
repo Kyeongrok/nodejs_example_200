cf412de0aab0b0bc63225fb8db5da0b548f46b9b
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-10-24 08:05:52 +0900

    158_cookie 쿠키분해

--- 00n_http/158_cookie2.js
+++ 00n_http/158_cookie2.js
@@ -1,28 +1,24 @@
 const http = require('http');
 
 http.createServer((request, response) => {
-
-    // GET cookie
+  // GET cookie
   if (request.headers.cookie) {
-      let cookie = request.headers.cookie.split(';').map(function (element) {
-          let element = element.split('=');
-          return {
-              key: element[0],
-              value: element[1]
-          };
-      });
-      response.end(`<h1>${JSON.stringify(cookie)}</h1>`);
-  }else {
-
-
-      // SET cookie
-      response.writeHead(200, {
-          'Content-Type': 'text/html',
-          'Set-Cookie': ['soju = grilledPork', 'beer = chicken']
-      });
-
-      // cookie output
-      response.end(`<h1>${'쿠키 생성함'}</h1>`);
+    const cookie = request.headers.cookie.split(';').map((element) => {
+      element = element.split('=');
+      return {
+        name: element[0],
+        value: element[1],
+      };
+    });
+    response.end(`<h1>${JSON.stringify(cookie)}</h1>`);
+  } else {
+    // SET cookie
+    response.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Set-Cookie': ['soju = grilledPork', 'beer = chicken'],
+    });
+    // cookie output
+    response.end(`<h1>${'쿠키 생성함'}</h1>`);
   }
 }).listen(50000, () => {
   console.log('서버가 동작 중입니다, http://127.0.0.1:50000');
