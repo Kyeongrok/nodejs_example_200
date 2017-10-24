8abae31a1bd5285252fa3bfcbf9309ace3fa04da
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-10-24 18:06:16 +0900

    116_ejs_module 기본형식, 117_ejs_module 데이터 전달

--- 00m_external_module/117_ejs_Module2.js
+++ 00m_external_module/117_ejs_Module2.js
@@ -1,1 +1,16 @@
-// 201709_JJH ejs module 소개
+
+const ejs = require('ejs');
+const fs = require('fs');
+const http = require('http');
+
+http.createServer((request, response) => {
+  fs.readFile('ejs_example2.ejs', 'utf-8', (error, data) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end(ejs.render(data, {
+      table_name: 'Multiplication table 19 X ?',
+      number: '19',
+    }));
+  });
+}).listen(50000, () => {
+  console.log('서버가 동작 중입니다, http://127.0.0.1:50000');
+});
