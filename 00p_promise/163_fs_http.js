5768accf1da9047b6246663976ea9c7f06f65656
kyeongrok.kim <kyeongrokkim@okiconcession.com>
2017-10-25 18:04:38 +0900

    번호 조정3

--- 00p_promise/163_fs_http.js
+++
@@ -1,9 +0,0 @@
-// fs모듈과 httpmodule 같이 써서사용 하기
-const http = require('http');
-const fs = require('fs');
-
-const server = http.createServer((req, res) => {
-  const instream = fs.createReadStream('./testoutput.txt');
-  instream.pipe(res);
-});
-server.listen(7001, '127.0.0.1');
