1017285221e067dd2cebe5c853ab67a0b0a5e16f
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-10-26 17:23:21 +0900

    126_express_1.js 모듈 첫 시간

--- 121to160_level4/126_express_1.js
+++ 121to160_level4/126_express_1.js
@@ -3,12 +3,14 @@
 // 익스프레스로 웹 서버 만들기
 
 const express = require('express');
-const http = require('http');
 
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.use((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.end('Hello express module');
+});
 
-http.createServer(app).listen(app.get('port'), () => {
-  console.log(`express server start :${app.get('port')}`);
+app.listen(50000, () => {
+  console.log('서버가 동작 중입니다, http://127.0.0.1:50000');
 });
