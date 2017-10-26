0f332adc128f539cff2bc8050cc2e192cdbcd01f
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-10-26 17:11:49 +0900

    120_pug_module3.Js 내용추가 및 pug_example3.pug 파일 내용 추가

--- 00m_external_module/120_pug_Module3.js
+++ 00m_external_module/120_pug_Module3.js
@@ -1,1 +1,17 @@
-//201709_JJH jade module, Node.js 프로그래밍 P.153
+// 201709_JJH jade module, Node.js 프로그래밍 P.153
+const pug = require('pug');
+const fs = require('fs');
+const http = require('http');
+
+http.createServer((request, response) => {
+  fs.readFile('pug_example3.pug', 'utf-8', (error, data) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    const fn = pug.compile(data);
+    response.end(fn({
+      table_name: 'Multiplication table 19 X ?',
+      number: '19',
+    }));
+  });
+}).listen(50000, () => {
+  console.log('서버가 동작 중입니다, http://127.0.0.1:50000');
+});
