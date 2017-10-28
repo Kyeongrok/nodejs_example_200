687b7af03ad5bd10431a2096b9ad9cae52b40fc5
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-10-28 19:35:40 +0900

    126_express_1, 127_express_2

--- 121to160_level4/126_express_1.js
+++ 121to160_level4/126_express_1.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use((request, response) => {
   response.send('Hello express module');
 });
 
