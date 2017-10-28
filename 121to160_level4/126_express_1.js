ead26e618ec9943de0b460d50bc007fc932ed8a0
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-10-28 18:42:32 +0900

    126_express_1, 127_express_2

--- 121to160_level4/126_express_1.js
+++ 121to160_level4/126_express_1.js
@@ -2,11 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end('Hello express module');
+app.get('/', (request, response) => {
+  response.send('Hello express module');
 });
 
-app.listen(50000, () => {
-  console.log('서버가 동작 중입니다, http://127.0.0.1:50000');
+app.listen(3000, () => {
+  console.log('Server is running port 3000!');
 });
--- 121to160_level4/127_express_2.js
+++ 121to160_level4/127_express_2.js
@@ -1,19 +1,16 @@
-// 201709_JJH
-// P.154, 미들웨어로 클라이언트 응답 보내기
 
- let express = require('express');
- let http = require('http');
+const express = require('express');
+const http = require('http');
 
- let app = express();
+const app = express();
 
-app.use(function(req, res, next){
+app.use((request, response, next) => {
   console.log('first middleware request');
 
-  res.writeHead('200', {'Content-Type':'text/html;charset=uft8'});
-  res.end('<h1>express server response</h1>');
-
+  response.writeHead('200', { 'Content-Type': 'text/html;charset=uft8' });
+  response.end('<h1>express server response</h1>');
 });
 
-http.createServer(app).listen(3000,function(){
+http.createServer(app).listen(3000, () => {
   console.log('express server started from 3000 port');
 });
