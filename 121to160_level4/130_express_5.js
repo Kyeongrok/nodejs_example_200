0a5d1c2c56965f9de5047170c9f468789ecfcc36
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-10-29 19:49:08 +0900

    130_express_5 static 부분 정리

--- 121to160_level4/130_express_5.js
+++ 121to160_level4/130_express_5.js
@@ -1,23 +1,17 @@
 // static 미들웨어
-
+// express 모듈 불러오기
 const express = require('express');
 
+// express 객체 생성
 const app = express();
 
-app.get('/', (request, response) => {
-  const result = [];
-  const multipleNumber = 9;
-  for (let i = 1; i < 5; i++) {
-    result.push({
-      number: `${multipleNumber}X${i}`,
-      multiple: multipleNumber * i,
-    });
-  }
-  response.send(result);
-});
-
-app.get('/error', (request, response) => {
-  response.status(404).send('404 ERROR');
+app.use(express.static(`${__dirname} + '/multimedia`));
+app.use((request, response) => {
+  // response.writeHead('200', { 'Content-Type': 'text/html;' });
+  response.writeHead('200', { 'Content-Type': 'image/jpeg' });
+  response.end('<img src="/newyork.jpg" />');
+  // response.writeHead(200, { 'Content-Type': 'audio/mp3' });
+  // response.end('./Cullah_DaftPunk.mp3');
 });
 
 app.listen(3000, () => {
