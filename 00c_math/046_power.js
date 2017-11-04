ff41e8ee386afb5fa02fbdbaabbd71f65bce4937
kyeongrok <oceanfog1@gmail.com>
2017-11-04 19:42:48 +0900

    pow 변수명 변경

--- 00c_math/046_power.js
+++ 00c_math/046_power.js
@@ -8,9 +8,9 @@ const pow6 = Math.pow(2, 0.5); // 1.4142135623730951 (square root of 2)
 const pow7 = Math.pow(2, 1 / 3); // 1.2599210498948732 (cube root of 2)
 
 const pow8 = Math.pow(7, -2); // 0.02040816326530612 (1/49)
-const pow1 = Math.pow(8, -1 / 3); // 0.5
-const pow9 = Math.pow(-7, 2); // 49 (squares are positive)
-const pow10 = Math.pow(-7, 3); // -343 (cubes can be negative)
-const pow11 = Math.pow(-7, 0.5); // NaN (negative numbers don't have a real square root)
-const pow12 = Math.pow(-7, 1 / 3); // NaN
+const pow9 = Math.pow(8, -1 / 3); // 0.5
+const pow10 = Math.pow(-7, 2); // 49 (squares are positive)
+const pow11 = Math.pow(-7, 3); // -343 (cubes can be negative)
+const pow12 = Math.pow(-7, 0.5); // NaN (negative numbers don't have a real square root)
+const pow13 = Math.pow(-7, 1 / 3); // NaN
 
