1deff3532376322dfcf8410bdc700a5823234e3f
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-11-07 15:51:40 +0900

    135_express_10.js

--- 121to160_level4/135_express_10.js
+++ 121to160_level4/135_express_10.js
@@ -1,10 +1,13 @@
 // connet-muliparty 미들웨어
+
+// 모듈 추출
 const express = require('express');
 const fs = require('fs');
 const multipart = require('connect-multiparty');
 
 const app = express();
 
+// 미들웨어 설정
 app.use(multipart({ uploadDir: `${__dirname}/upload` }));
 
 app.get('/', (request, response) => {
@@ -17,7 +20,8 @@ app.post('/', (request, response) => {
   const imgFile = request.files.image;
   const outputPath = `${__dirname}/upload/${Date.now()}_${imgFile.name}`;
   console.log(outputPath);
-  console.log(request.body, request.files);
+  console.log(request.body);
+  console.log(request.files);
   fs.rename(imgFile.path, outputPath, () => {
     response.redirect('/');
   });
