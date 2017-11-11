8a6a9da69462195a18436942ce34e38fd772244e
kyeongrok <oceanfog1@gmail.com>
2017-11-11 22:19:23 +0900

    sort2

---
+++ 00h_array_operation/083_sort_options.js
@@ -0,0 +1,18 @@
+const studentList = [
+  { name: 'kyeongrok', age: 31, math: 85, english: 87 },
+  { name: 'jihyun', age: 31, math: 95, english: 97 },
+  { name: 'minsup', age: 35, math: 76, english: 84 },
+  { name: 'dasom', age: 35, math: 84, english: 73 },
+  { name: 'yuna', age: 26, math: 54, english: 67 },
+  { name: 'mattheue', age: 29, math: 34, english: 100 },
+];
+
+studentList.sort((beforeStudent, nextStudent) => {
+  if (beforeStudent.age > nextStudent.age) return 1;
+  else if (beforeStudent.age < nextStudent.age) return -1;
+  else if (beforeStudent.math > nextStudent.math) return -1;
+  else if (beforeStudent.math < nextStudent.math) return 1;
+  return 0;
+});
+
+console.log(studentList);
