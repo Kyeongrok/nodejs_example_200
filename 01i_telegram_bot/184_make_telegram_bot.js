bf47958653e0e3873d1999ee69f586e1eee08fa9
kyeongrok.kim <kyeongrokkim@okiconcession.com>
2017-11-23 19:58:23 +0900

    telegram bot 추가

--- 01i_telegram_bot/184_make_telegram_bot.js
+++ 01i_telegram_bot/184_make_telegram_bot.js
@@ -1,1 +1,20 @@
-// botFather로 telegram bot 만들기
+const Bot = require('node-telegram-bot-api');
+
+const bot = new Bot('281761192:AAE7h61HIio8eviXggpssYHrJJ58nHWT32A', { polling: true });
+
+const onChatMessage = (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'oh, hello', {
+    disable_notification: true,
+  }).then(() => {
+    // reply sent!
+  });
+}
+
+bot.on('message', (msg) => {
+  console.log(msg);
+  if (msg.text) {
+    return onChatMessage(msg);
+  }
+});
+
