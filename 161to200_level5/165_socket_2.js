7bee97b6f84d3dbfdbb0442ecda565769a9f36a8
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-11-28 01:39:58 +0900

    164_socket_1.js, 165.socket_2.js express 모듈로 해서 수정

--- 161to200_level5/165_socket_2.js
+++ 161to200_level5/165_socket_2.js
@@ -10,11 +10,13 @@ const io = require('socket.io')(server);
 io.on('connection', (client) => {
   console.log('Client connection');
 
+  // 이벤트 연결 및 전달
   client.on('clientmsg', (data) => {
     console.log('This is client Data:', data);
     client.emit('msg', data);
   });
 
+  // 이벤트 연결 및 전달
   client.on('status', () => {
     console.log('Status event(Server)');
     setInterval(() => {
