d84858bee96ccf45d2ffa529d363c077a0d02944
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-12-03 20:27:45 +0900

    185_MySQL_create.js SQL문으로 업로드

--- numberplus/158_MySQL_create.js
+++ numberplus/158_MySQL_create.js
@@ -13,8 +13,13 @@ const connection = mysql.createConnection({
 // 데이터베이스 연결
 connection.connect();
 
-// Select 쿼리문 사용
+// create 쿼리문 사용
 connection.query('create table books (number INT NOT NULL AUTO_INCREMENT PRIMARY KEY, genre VARCHAR(20) NOT NULL, name VARCHAR(50) NOT NULL, writer VARCHAR(30) NOT NULL, releasedate date NOT NULL);', (error, results, fields) => {
+  if (error) throw error;
+  console.log(results);
+});
+
+connection.query('describe books', (error, results, fields) => {
   if (error) throw error;
   console.log(results);
 });
