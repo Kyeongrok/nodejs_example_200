7d04123abf03c9cc25b3da5c0a7fd6eb64c3febe
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-12-06 11:18:38 +0900

    185_MySQL_create.js SQL문으로 업로드

--- 121to160_level4/159_MySQL_insert.js
+++ 121to160_level4/159_MySQL_insert.js
@@ -13,8 +13,8 @@ const connection = mysql.createConnection({
 // 데이터베이스 연결
 connection.connect();
 
-// Select 쿼리문 사용
-connection.query('SELECT * from books', (error, results, fields) => {
+// Insert 쿼리문 사용
+connection.query('insert into books (genre, name, writer, releasedate) values (\'fantasy\', \'LUMINE\', \'Emma Krogell\', \'2015-05-15\'), (\'comedy\', \'Mygiant Nerd Boyfriend\', \'fishball\', \'2017-03-03\'), (\'romance\', \'I Love Yoo\', \'Quimchee\', \'2016-08-21\'), (\'action\', \'Tower of God\', \'SIU\', \'2017-10-01\'), (\'action\', \'Rise from Ashes\', \'Madeleine Rosca\', \'2016-01-13\');', (error, results, fields) => {
   if (error) throw error;
   console.log(results);
 });
