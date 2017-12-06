6445f1462f3723e672300d06de624f0de8c86388
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-12-06 16:24:57 +0900

    185_MySQL_create.js SQL문으로 업로드

--- 121to160_level4/160_MySQL_select.js
+++ 121to160_level4/160_MySQL_select.js
@@ -13,8 +13,44 @@ const connection = mysql.createConnection({
 // 데이터베이스 연결
 connection.connect();
 
-// Select 쿼리문 사용
-connection.query('SELECT * from books', (error, results, fields) => {
+// Select 쿼리문 (* 사용)
+connection.query('select * from books', (error, results, fields) => {
+  if (error) throw error;
+  console.log(results);
+});
+
+// Select 쿼리문 (각각의 필드 명칭) 사용
+connection.query('select number, genre, name, writer, releasedate from books', (error, results, fields) => {
+  if (error) throw error;
+  console.log(results);
+});
+
+// Select 쿼리문 (where문 사용)
+connection.query('select * from books where genre = \'action\'', (error, results, fields) => {
+  if (error) throw error;
+  console.log(results);
+});
+
+// Select 쿼리문 (where문 사용 - or)
+connection.query('select * from books where genre = \'action\' or genre = \'comedy\'', (error, results, fields) => {
+  if (error) throw error;
+  console.log(results);
+});
+
+// Select 쿼리문 (like 사용)
+connection.query('select * from books where releasedate LIKE \'2017%\'', (error, results, fields) => {
+  if (error) throw error;
+  console.log(results);
+});
+
+// Select 쿼리문 (order by)
+connection.query('select number, genre, name, writer, releasedate from books order by releasedate;', (error, results, fields) => {
+  if (error) throw error;
+  console.log(results);
+});
+
+// Select 쿼리문 (order by desc)
+connection.query('select number, genre, name, writer, releasedate from books order by releasedate desc;', (error, results, fields) => {
   if (error) throw error;
   console.log(results);
 });
