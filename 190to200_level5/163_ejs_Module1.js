980791f8e14a31bcc83428556c37b305cc4bf42a
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-12-10 16:44:36 +0900

    목차 수정

--- 190to200_level5/163_ejs_Module1.js
+++ 190to200_level5/163_ejs_Module1.js
@@ -3,12 +3,9 @@ const fs = require('fs');
 const http = require('http');
 
 http.createServer((request, response) => {
-  fs.readFile('ejs_example2.ejs', 'utf-8', (error, data) => {
+  fs.readFile('163_ejs_example.ejs', 'utf-8', (error, data) => {
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end(ejs.render(data, {
-      table_name: 'Multiplication table 19 X ?',
-      number: '19',
-    }));
+    response.end(ejs.render(data));
   });
 }).listen(50000, () => {
   console.log('서버가 동작 중입니다, http://127.0.0.1:50000');
