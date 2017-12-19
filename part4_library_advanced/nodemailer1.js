4d8f1d83c983fa29d1fcd71dcf60acd034d52201
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-12-19 16:02:26 +0900

    nodemailer1 추가

--- part4_library_advanced/nodemailer1.js
+++ part4_library_advanced/nodemailer1.js
@@ -6,19 +6,15 @@ const transporter = nodemailer.createTransport({
   auth: {
     // user: '지메일아이디@gmail.com',
     // pass: '비밀번호'
-    user: 'stophyuni@gmail.com',
-    pass: 'gosdkdi1',
   },
 });
 
 // setup email data with unicode symbols
 const mailOptions = {
-  // from: '지메일아이디@gmail.com', // sender address
-  from: 'stophyuni@gmail.com', // sender address
-  to: 'stophyuni@gmail.com', // list of receivers
+  from: '지메일아이디@gmail.com', // sender address
+  to: '지메일아이디@gmail.com', // list of receivers
   subject: 'Hello ✔', // Subject line
   text: 'Hello world?', // plain text body
-  // html: '<b>Hello world?</b>', // html body
 };
 
 // send mail with defined transport object
