b8ddb84f8c08f8108000d014911c9bb34aba7a4e
AzureAD\JihyunJeong <stophyuni@gmail.com>
2017-12-20 15:35:02 +0900

    nodemailer 2,3 추가

--- part4_library_advanced/nodemailer3.js
+++ part4_library_advanced/nodemailer3.js
@@ -4,19 +4,29 @@ const transporter = nodemailer.createTransport({
   service: 'Gmail',
   auth: {
     user: '지메일아이디@gmail.com',
-    pass: '비밀번호'
+    pass: '비밀번호',
   },
 });
 
 // setup email data with unicode symbols
 const mailOptions = {
+
   from: '지메일아이디@gmail.com', // sender address
   to: '지메일아이디@gmail.com', // list of receivers
-  subject: 'Hello HTML', // Subject line
+  subject: 'Hello attachment', // Subject line
   // text: 'Hello world?', // plain text body
+
   // html body
-  html: '<h1>Hello HTML</h1><a href="http://www.infopub.co.kr">' +
-  '<img src="http://www.infopub.co.kr/pdspool/common/main_top/2016-11-02.jpg"/></p></a>',
+  html: '<h1>Hello Attachment</h1><a href="http://www.infopub.co.kr">' +
+    '<img src="http://www.infopub.co.kr/pdspool/common/main_top/2016-11-02.jpg"/></p></a>',
+
+  // attachment configuration
+  attachments: [
+    {
+      filename: 'attachment_test.xlsx',
+      path: 'attachment_test.xlsx',
+    },
+  ],
 };
 
 // send mail with defined transport object
@@ -25,6 +35,7 @@ transporter.sendMail(mailOptions, (error, info) => {
     console.log(error);
   } else {
     console.log(`Message sent: ${info.response}`);
+    console.log(mailOptions.attachments);
   }
   transporter.close();
 });
